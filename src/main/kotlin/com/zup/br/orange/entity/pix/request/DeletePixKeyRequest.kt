7bbf56db24295949d0c415b8d7960b8ca7781751
guipernicone <guipernicone@hotmail.com>
2021-04-13 18:05:58 -0300

    Altera tipo de validation

--- src/main/kotlin/com/zup/br/orange/entity/pix/request/DeletePixKeyRequest.kt
+++ src/main/kotlin/com/zup/br/orange/entity/pix/request/DeletePixKeyRequest.kt
@@ -2,9 +2,10 @@ package com.zup.br.orange.entity.pix.request
 
 import io.micronaut.core.annotation.Introspected
 import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotNull
 
 @Introspected
 data class DeletePixKeyRequest (
-    @field: NotBlank val pixId: Long,
+    @field: NotNull val pixId: Long,
     @field: NotBlank val clientId: String
 )
